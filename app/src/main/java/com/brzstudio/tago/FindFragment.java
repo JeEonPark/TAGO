2f749c866d6f17fd0b12f7d203ef96681c747f15
JeEonPark <ggungtoo@icloud.com>
2022-05-19 19:19:15 +0900

    카카오 지도 추가

--- app/src/main/java/com/brzstudio/tago/FindFragment.java
+++ app/src/main/java/com/brzstudio/tago/FindFragment.java
@@ -1,32 +1,32 @@
 package com.brzstudio.tago;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+
+import net.daum.mf.map.api.MapView;
 
 
 public class FindFragment extends Fragment implements View.OnClickListener {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find, container, false);
 
-        Button signoutButton = (Button) view.findViewById(R.id.signoutButton);
 
-        signoutButton.setOnClickListener(this);
+        MapView mapView = new MapView(getActivity());
+
+        ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
 
         return view;
     }
