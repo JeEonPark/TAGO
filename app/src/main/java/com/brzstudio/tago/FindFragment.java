fd2de81284b795367699ef88ca79c59f85c6e495
JeEonPark <ggungtoo@icloud.com>
2022-05-27 12:05:58 +0900

    카카오 지도 추가

--- app/src/main/java/com/brzstudio/tago/FindFragment.java
+++ app/src/main/java/com/brzstudio/tago/FindFragment.java
@@ -2,10 +2,13 @@ package com.brzstudio.tago;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -22,7 +25,8 @@ public class FindFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find, container, false);
 
-
+        //화면 크기에 맞게 지도 크기 변경
+        setMapSize(view);
         MapView mapView = new MapView(getActivity());
 
         ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
@@ -32,7 +36,6 @@ public class FindFragment extends Fragment implements View.OnClickListener {
     }
 
     public void onClick(View view) {
-
         switch (view.getId()) {
             case R.id.signoutButton:
                 FirebaseAuth.getInstance().signOut();
@@ -42,6 +45,27 @@ public class FindFragment extends Fragment implements View.OnClickListener {
                 f.finish();
 
         }
+    }
+
+    public void setMapSize(View view) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int widthPixels = metrics.widthPixels;
+        int heightPixels = metrics.heightPixels - dpToPx(340);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(widthPixels, heightPixels);
+
+        view.findViewById(R.id.map_view).setLayoutParams(params);
+
+    }
+
+
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public int pxToDp(int px) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
 }
