ee1a7cbf4f01956cec11dfe95483fb44d1dd7b86
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-06-02 01:12:56 +0900

    채팅 리스트뷰

--- app/src/main/java/com/brzstudio/tago/ChatFragment.java
+++ app/src/main/java/com/brzstudio/tago/ChatFragment.java
@@ -12,21 +12,40 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class ChatFragment extends Fragment {
 
     public class ChatListItem{
-        private String departureText;
-        private String lastChatText;
+//        private String oldProfilePic;
+        private String oldDepartureText;
+        private String oldDestinationText;
+        private String oldLastChatText;
+        private String oldLivePeople;
+        private String oldTotalPeople;
 
-        public String getDepartureText() { return departureText; }
-        public void setDepartureText(String departureText) { this.departureText = departureText; }
-        public String getLastChatText() { return lastChatText; }
-        public void setLastChatText(String lastChatText) { this.lastChatText = lastChatText; }
-        ChatListItem(String departureText, String lastChatText) {
-            this.departureText = departureText;
-            this.lastChatText = lastChatText;
+//        public String getOldProfilePic() { return oldProfilePic; }
+//        public void setOldProfilePic(String oldProfilePic) { this.oldProfilePic = oldProfilePic; }
+        public String getOldDepartureText() { return oldDepartureText; }
+        public void setOldDepartureText(String oldDepartureText) { this.oldDepartureText = oldDepartureText; }
+        public String getOldDestinationText() { return oldDestinationText; }
+        public void setOldDestinationText(String oldDestinationText) { this.oldDestinationText = oldDestinationText; }
+        public String getOldLastChatText() { return oldLastChatText; }
+        public void setOldLastChatText(String oldLastChatText) { this.oldLastChatText = oldLastChatText; }
+        public String getOldLivePeople() { return oldLivePeople; }
+        public void setOldLivePeople(String oldLivePeople) { this.oldLivePeople = oldLivePeople; }
+        public String getOldTotalPeople() { return oldTotalPeople; }
+        public void setOldTotalPeople(String oldTotalPeople) { this.oldTotalPeople = oldTotalPeople; }
+
+        ChatListItem(String oldDepartureText, String oldDestinationText, String oldLastChatText, String oldLivePeople, String oldTotalPeople) {
+//            this.oldProfilePic = oldProfilePic;
+            this.oldDepartureText = oldDepartureText;
+            this.oldDestinationText = oldDestinationText;
+            this.oldLastChatText = oldLastChatText;
+            this.oldLivePeople = oldLivePeople;
+            this.oldTotalPeople = oldTotalPeople;
         }
     }
     public class ChatListItemAdapter extends BaseAdapter {
@@ -57,11 +76,19 @@ public class ChatFragment extends Fragment {
                 LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 convertView = inflater.inflate(R.layout.listview_fragment_chat, parent, false);
             }
-            TextView departureTextText = convertView.findViewById(R.id.departureText);
-            TextView lastChatTextText = (TextView) convertView.findViewById(R.id.lastChatText);
 
-            departureTextText.setText(chatListItem.getDepartureText());
-            lastChatTextText.setText(chatListItem.getLastChatText());
+            TextView oldDepartureTextText = convertView.findViewById(R.id.oldDepartureText);
+            TextView oldDestinationTextText = convertView.findViewById(R.id.oldDestinationText);
+            TextView oldLastChatTextText = convertView.findViewById(R.id.oldLastChatText);
+            TextView oldLivePeopleTextText = convertView.findViewById(R.id.oldLivePeople);
+            TextView oldTotalPeopleTextText = convertView.findViewById(R.id.oldTotalPeople);
+
+            oldDepartureTextText.setText(chatListItem.getOldDepartureText());
+            oldDestinationTextText.setText(chatListItem.getOldDestinationText());
+            oldLastChatTextText.setText(chatListItem.getOldLastChatText());
+            oldLivePeopleTextText.setText(chatListItem.getOldLivePeople());
+            oldTotalPeopleTextText.setText(chatListItem.getOldTotalPeople());
+
             return convertView;
         }
         public void addItem(ChatListItem item) {
@@ -79,9 +106,14 @@ public class ChatFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_chat,container,false);
         adapter = new ChatListItemAdapter();
 
-        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "충남 천안시 동남구 상명대길 31"));
-        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "충남 천안시 동남구 상명대길 31"));
-        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "충남 천안시 동남구 상명대길 31"));
+        listView = v.findViewById(R.id.list);
+
+        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "탐스 피씨 카페", "피방 가자", "2", "3"));
+        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "천안역", "야우리 가쉴?", "1", "2"));
+        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "힐스테이트 광교", "집 가자", "2", "2"));
+        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "탐스 피씨 카페", "피방 가자", "2", "3"));
+        adapter.addItem(new ChatListItem("상명대학교 천안캠퍼스", "탐스 피씨 카페", "피방 가자", "2", "3"));
+        listView.setAdapter(adapter);
 
         // Inflate the layout for this fragment
         return v;
