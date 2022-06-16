02554d44606416883b8dc4874b2e85b4a24cf5a7
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-06-16 10:27:24 +0900

    logout 구현

--- app/src/main/java/com/brzstudio/tago/SettingFragment.java
+++ app/src/main/java/com/brzstudio/tago/SettingFragment.java
@@ -1,19 +1,37 @@
 package com.brzstudio.tago;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class SettingFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SettingFragment extends Fragment implements View.OnClickListener {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        Button signoutButton = (Button) view.findViewById(R.id.signoutButton);
+        signoutButton.setOnClickListener(this);
+
+        return view;
+    }
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.signoutButton:
+                FirebaseAuth.getInstance().signOut();
+                FragmentActivity f = getActivity();
+                Intent intent = new Intent(f, LoginActivity.class);
+                startActivity(intent);
+                f.finish();
+        }
     }
 }
